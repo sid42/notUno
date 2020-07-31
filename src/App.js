01bf49039e24680bc300e157afb03d319680ce12
sid42 <s34bhatt@uwaterloo.ca>
2020-07-31 09:34:18 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Peer from 'peerjs'
 
 function App() {
+  var initPeer
+  var initPeerConns = [] 
+  function create(){
+      initPeer = new Peer()
+
+      initPeer.on('open', (data) => {
+        console.log('initPeer made')
+        console.log(initPeer)
+      })
+      initPeer.on('connection', (conn) => {
+        initPeerConns.push(conn)
+        console.log('connection made in initPeer')
+        console.log((conn))
+        conn.on('data', (data) => {
+          console.log('received from peer ' + data)
+          conn.send('hiya!')
+        })
+      })
+    }
+
+  function join(peerIdx){ 
+      var peer = new Peer()
+      
+      peer.on('open', (data) => {
+        console.log('peer ' + peerIdx + ' created')
+        var conn = peer.connect(initPeer.id, {
+          reliable: true
+        })
+
+        conn.on('open', () => {
+          console.log('connection between peer ' + peerIdx + ' and initPeer made')
+          console.log(conn)
+          conn.on('data', (data) => {
+            console.log('received from initPeer ' + data + ' to peer ' + peerIdx)
+          })
+          conn.send('peer ' + peerIdx + ' says hello!')
+        })
+      })
+      
+      console.log(peer)
+  }
+
+  create()
+  initPeer.on('open', () => {
+    join(1)
+    join(2)
+  })
+  
+  setTimeout(() => { 
+    console.log('printing connections')
+    console.log(initPeerConns)
+    initPeerConns.forEach((conn) => {
+      conn.send('hello from iterator')
+      console.log(conn.peer)
+    })
+  }, 5000)
+
+
   return (
     <div className="App">
       <header className="App-header">
